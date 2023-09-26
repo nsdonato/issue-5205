b9e39bc99a488acf6a23758fa7d8d58d7026baa6
Noelia Donato <noeliadonato@gmail.com>
2023-09-26 23:53:03 +0200

    initial commit

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,12 +1,12 @@
-import { component$ } from "@builder.io/qwik";
-import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik'
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city'
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-  const head = useDocumentHead();
-  const loc = useLocation();
+  const head = useDocumentHead()
+  const loc = useLocation()
 
   return (
     <>
@@ -27,6 +27,10 @@ export const RouterHead = component$(() => {
       {head.styles.map((s) => (
         <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
+
+      {head.scripts.map((s) => (
+        <script key={s.key} {...s} />
+      ))}
     </>
-  );
-});
+  )
+})
